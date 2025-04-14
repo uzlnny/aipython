03793dc9c58a8b3956ee156b5e0eded7031e6eab
zlnny u <xxin0818@icloud.com>
2025-04-14 05:12:03 +0000

    dfd

--- mm.py
+++ mm.py
@@ -5,7 +5,7 @@ import random
 # 스포티파이 차트 페이지 URL
 url = 'https://charts.spotify.com/home'  # 스포티파이의 최신 차트 URL로 확인 필요
 
-# 헤더 설정 (멜론은 User-Agent 확인을 통해 봇 접근을 차단할 수 있으므로 설정이 필요할 수 있음)
+# 헤더 설정 (스포티파이는 User-Agent 확인을 통해 봇 접근을 차단할 수 있으므로 설정이 필요할 수 있음)
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
 }
